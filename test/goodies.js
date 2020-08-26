12c98bc5ebdbfe9a861e53703b18fc7c669e6d6c
Marcus Pöhls <marcus.poehls@gmail.com>
2020-08-26 09:13:46 +0200

    move to jest

--- test/goodies.js
+++ test/goodies.js
@@ -1,104 +1,100 @@
 'use strict'
 
-const Lab = require('@hapi/lab')
-const { expect } = require('@hapi/code')
 const { tap, upon, isPromise, isAsyncFunction, ifNullish } = require('..')
-
-const { describe, it } = exports.lab = Lab.script()
 
 describe('Goodies', () => {
   it('tap', async () => {
-    expect(tap(1)).to.equal(1)
+    expect(tap(1)).toEqual(1)
 
     expect(
       tap(new User('Marcus'), (user) => {
         user.setName('Goodie')
       })
-    ).to.equal({ name: 'Goodie' })
+    ).toEqual({ name: 'Goodie' })
 
     expect(
       tap([1, 2, 3], (items) => items.map(item => item * 2))
-    ).to.equal([1, 2, 3])
+    ).toEqual([1, 2, 3])
 
     expect(
       tap(await Promise.resolve([1, 2, 3]), (items) => {
         items.sort((a, b) => b - a)
       })
-    ).to.equal([3, 2, 1])
+    ).toEqual([3, 2, 1])
 
     // resolves a promise before passing it down to the callback
     expect(
       await tap(Promise.resolve(new User('Marcus')), (user) => user.setName('Goodie'))
-    ).to.equal({ name: 'Goodie' })
+    ).toEqual({ name: 'Goodie' })
 
     expect(
       await tap([1, 2, 3], async () => {
         await Promise.resolve()
       })
-    ).to.equal([1, 2, 3])
+    ).toEqual([1, 2, 3])
 
     expect(
       await tap(Promise.resolve([1, 2, 3]))
-    ).to.equal([1, 2, 3])
+    ).toEqual([1, 2, 3])
   })
 
   it('tap - handles second non-function arguments', async () => {
-    expect(tap(1, new User())).to.equal(1)
+    expect(tap(1, new User())).toEqual(1)
 
     expect(
       await tap(Promise.resolve('Supercharge'), 1234)
-    ).to.equal('Supercharge')
+    ).toEqual('Supercharge')
   })
 
   it('upon', async () => {
     // upon async
-    expect(await upon(1, async () => { })).to.be.undefined()
-    expect(await upon(Promise.resolve(1), new User())).to.equal(1)
+    expect(await upon(1, async () => { })).toBeUndefined()
+    expect(await upon(Promise.resolve(1), new User())).toEqual(1)
 
     // resolves a promise before passing it down to the callback
     expect(
       await upon(Promise.resolve(new User('Marcus')), (user) => {
         return user.getName()
       })
-    ).to.equal('Marcus')
+    ).toEqual('Marcus')
 
     expect(
       await upon(Promise.resolve(new User('Marcus')))
-    ).to.equal(new User('Marcus'))
+    ).toEqual(new User('Marcus'))
 
     // upon sync
-    expect(upon(1)).to.equal(1)
-    expect(upon(1, new User())).to.equal(1)
+    expect(upon(1)).toEqual(1)
+    expect(upon(1, new User())).toEqual(1)
 
     expect(
       upon(new User('Marcus'), (user) => {
         return user.getName()
       })
-    ).to.equal('Marcus')
+    ).toEqual('Marcus')
   })
 
   it('isPromise', () => {
-    expect(isPromise()).to.be.false()
-    expect(isPromise(1)).to.be.false()
-    expect(isPromise('no')).to.be.false()
+    expect(isPromise()).toBe(false)
+    expect(isPromise(1)).toBe(false)
+    expect(isPromise('no')).toBe(false)
 
     async function asyncFn () {
       await new Promise(resolve => setTimeout(resolve, 1))
     }
 
-    expect(isPromise(asyncFn())).to.be.true()
-    expect(isPromise(new Promise(() => {}))).to.be.true()
+    expect(isPromise(asyncFn())).toBe(true)
+    expect(isPromise(new Promise(() => {}))).toBe(true)
   })
 
   it('isAsyncFunction', () => {
-    expect(isAsyncFunction(1)).to.be.false()
-    expect(isAsyncFunction('no')).to.be.false()
-    expect(isAsyncFunction(null)).to.be.false()
-    expect(isAsyncFunction(undefined)).to.be.false()
-    expect(isAsyncFunction(function () { })).to.be.false()
-    expect(isAsyncFunction(new Promise(() => {}))).to.be.false()
+    expect(isAsyncFunction(1)).toBe(false)
+    expect(isAsyncFunction('no')).toBe(false)
+    expect(isAsyncFunction(null)).toBe(false)
+    expect(isAsyncFunction(undefined)).toBe(false)
+    expect(isAsyncFunction(function () { })).toBe(false)
+    expect(isAsyncFunction(new Promise(() => {}))).toBe(false)
 
-    expect(isAsyncFunction(async function () {})).to.be.true()
+    expect(isAsyncFunction(async function () {})).toBe(true)
   })
 
   it('ifNullish', async () => {
@@ -106,31 +102,31 @@ describe('Goodies', () => {
       ifNullish(null, () => {
         return 'is-null'
       })
-    ).to.equal('is-null')
+    ).toEqual('is-null')
 
     expect(
       ifNullish(undefined, () => {
         return 'is-undefined'
       })
-    ).to.equal('is-undefined')
+    ).toEqual('is-undefined')
 
     expect(
       ifNullish(0, () => {
         return 'zero'
       })
-    ).to.be.undefined()
+    ).toBeUndefined()
 
     expect(
       ifNullish('', () => {
         return 'empty-string'
       })
-    ).to.be.undefined()
+    ).toBeUndefined()
 
     expect(
       await ifNullish(null, async () => {
         return 'Promise'
       })
-    ).to.equal('Promise')
+    ).toEqual('Promise')
   })
 })
 
