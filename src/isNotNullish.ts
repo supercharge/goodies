95ab29730d90c8d8b5f272d90498567148ed127c
Marcus Pöhls <mp@seda.digital>
2021-08-19 14:42:19 +0200

    add isNotNullish

---
+++ src/isNotNullish.ts
@@ -0,0 +1,23 @@
+'use strict'
+
+import { isNullish } from './isNullish'
+
+/**
+ * Determine whether the given `input` is **not** `null` or `undefined`.
+ *
+ * @param {T} input
+ *
+ * @returns {Boolean}
+ *
+ * @example
+ * isNotNullish() // false
+ * isNotNullish(null) // false
+ * isNotNullish(undefined) // false
+ *
+ * isNotNullish(1) // true
+ * isNotNullish('') // true
+ * isNotNullish(false) // true
+ */
+export function isNotNullish<T> (input: T | undefined | null): input is T {
+  return !isNullish(input)
+}
